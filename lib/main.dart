7b7175a9a16009cef1caf3006db467e6afc5222a
SekharJr <amalsekhar1@gmail.com>
2023-04-28 19:16:49 +0530

    trvlon loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:travlon/pages/addOns.dart';
+import 'package:travlon/pages/duo3.dart';
+import 'package:travlon/pages/duo4.dart';
+import 'package:travlon/pages/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +18,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return ChangeNotifierProvider(
+      create: (context)=>Helper(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
 
-        primarySwatch: Colors.blue,
+          primarySwatch: Colors.blue,
+        ),
+        home: iconreturn(next: Icons.location_on_outlined,),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
